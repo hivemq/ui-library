26567b2dfe25aa905a95b92859d2126278fe5af4
oli-hivemq <olivier.frebourg@hivemq.com>
2024-09-04 14:00:53 +0200

    bump: ui-theme version

--- .storybook/chakraTheme.ts
+++ .storybook/chakraTheme.ts
@@ -1,11 +1,6 @@
 // Theming with Chakra UI is based on the Styled System Theme Specification
 // Extend the theme to include custom colors, fonts, etc
 import { extendTheme } from "@chakra-ui/react";
-import { colors, components, fonts, styles } from "@hivemq/ui-theme";
+import { theme } from "@hivemq/ui-theme";
 
-export const chakraTheme = extendTheme({
-  styles,
-  fonts,
-  colors,
-  components,
-});
+export const chakraTheme = extendTheme(theme);
